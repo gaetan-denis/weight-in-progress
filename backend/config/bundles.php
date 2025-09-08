d4cebf9891bd785d9558065ce19122a7c9fe82ed
Gaëtan Denis <gaetandenis@outlook.be>
2025-09-08 18:15:12 +0200

    chore(dev): add symfony/maker-bundle and security
    - Installed symfony/maker-bundle for generating entities, controllers,
      etc.
      - Installed symfony/security-bundle for authentication and
        authorization

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
